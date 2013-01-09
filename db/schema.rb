1e33167093cbe815714c3d9d07fa56d5234cd8eb
stefanledin <stefanledin@gmail.com>
2013-01-09 07:51:52 +0100

    La till :till och :fran

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130107210613) do
+ActiveRecord::Schema.define(:version => 20130106133654) do
 
   create_table "presets", :force => true do |t|
     t.string   "label"
@@ -35,8 +35,6 @@ ActiveRecord::Schema.define(:version => 20130107210613) do
     t.boolean  "ankomstitid"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
-    t.string   "fran"
-    t.string   "till"
   end
 
   create_table "users", :force => true do |t|
