2cdc390e9ea34569f6287a2d8c110cd4cbe126bd
stefanledin <stefan.ledin@gmail.com>
2013-01-09 06:19:10 +0100

    Byggt formulär och funktioner för att spara i db

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -9,14 +9,31 @@ class TripsController < ApplicationController
 	end
 
 	def create
+		
+		if params[:avgangtabell] == params[:avgangverklig]
+			avGangITID = true
+		else
+			avGangITID = false
+		end
+
+		if params[:ankomsttabell] == params[:ankomstverklig]
+			ankomstITID = true
+		else
+			ankomstITID = false
+		end
+			
 		Trip.create({
 			:datum => params[:datum],
 			:fran => params[:fran],
 			:till => params[:till],
+			:avgangitid => avGangITID,
 			:avgangtabell => params[:avgangtabell],
 			:avgangverklig => params[:avgangverklig],
+			:ankomstitid => ankomstITID,
 			:ankomsttabell => params[:ankomsttabell],
 			:ankomstverklig	 => params[:ankomstverklig]
 		})
+
+		redirect_to '/'
 	end
 end
