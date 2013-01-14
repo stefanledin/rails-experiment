f413a6a9d8e4a1debb4451e3e805bd9a87eabd12
stefanledin <stefan.ledin@gmail.com>
2013-01-14 06:13:10 +0100

    Nu kan man logga in med facebook

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,46 @@
 class HomeController < ApplicationController
 
-  def index
-  	@presets = Preset.all
-  	@trips = Trip.all
-  end
+	require 'koala'
+
+	def index
+		@oauth = Koala::Facebook::OAuth.new(128327427333621,'30fb84285d1f9c0013f9614cead58d24', root_url )
+		
+		# När man skickas tillbaka från inloggningsrutan 
+		if params[:code] && !session[:fb_user]
+			@graph = Koala::Facebook::API.new(@oauth.get_access_token(params[:code]))
+			session[:fb_user] = @graph.get_object('me')
+		end
+
+		if session[:fb_user]
+			@user = User.where("user_id = ?", session[:fb_user]['id']).first
+			if @user.nil?
+				redirect_to '/register'
+			end
+			
+			@presets = @user.presets.all
+			@trips = @user.trips.all
+		end
+
+	end
+
+	def login
+	end
+
+	def register
+		@user = User.create({
+			:first_name => session[:fb_user]['first_name'],
+			:last_name => session[:fb_user]['last_name'],
+			:user_id => session[:fb_user]['id']	
+		})
+		if @user.save
+			redirect_to root_url						
+		end
+	end
+
+	def logout
+		#session[:access_token] = nil
+		session[:fb_user] = nil
+		redirect_to root_url
+	end
 
 end
