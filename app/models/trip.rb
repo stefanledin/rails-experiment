8dee1b9facdf0de08ce78d337e5be8ae5eb9d49f
Stefan Ledin <stefan.ledin@gmail.com>
2013-01-16 20:59:09 +0100

    diff1 och diff2 tillagda som tillgängliga attribut

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,4 @@
 class Trip < ActiveRecord::Base
-  attr_accessible :till, :ankomstitid, :ankomsttabell, :ankomstverklig, :fran, :avgangitid, :avgangtabell, :avgangverklig, :datum, :user_id
+  attr_accessible :till, :ankomstitid, :ankomsttabell, :ankomstverklig, :fran, :avgangitid, :avgangtabell, :avgangverklig, :datum, :user_id, :diff1, :diff2
   belongs_to :user
 end
