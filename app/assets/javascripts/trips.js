700a858564e85340cc0aaf934546d37485a85a8b
Stefan Ledin <stefan.ledin@gmail.com>
2013-01-18 06:33:50 +0100

    Påbörjat "nu-funtionen"

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -1,6 +1,10 @@
 [].forEach.call(document.querySelectorAll('button.now'), function (el) {
 	el.addEventListener('click', function (e) {
 		e.preventDefault();
-		console.log(el);
+		var textField = document.querySelector('input#'+this.dataset.now),
+		t = new Date(),
+		hour = (t.getHours() < 10) ? '0' + t.getHours() : t.getHours();
+		console.log(hour);
+		textField.value="'+hour+'";
 	});
 });
